607524fd400540eab48babc12a144b30092e2690
FeltSense <feltsense62@gmail.com>
2025-09-19 21:40:33 -0300

    Auto-fix build errors

--- next.config.js
+++ next.config.js
@@ -6,9 +6,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
     domains: ['images.unsplash.com', 'cdn.pixabay.com', 'pixabay.com']
-  },
-  // Fixed: Remove assetPrefix for Vercel deployment
-  // assetPrefix causes font loading issues on Vercel
+  }
 }
 
 module.exports = nextConfig
